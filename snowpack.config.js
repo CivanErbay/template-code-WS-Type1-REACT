8ccc5469a1cf06d4c102040113aa9fee145c7b36
Civan <civan.erbay@web.de>
2021-02-07 17:36:33 +0100

    Add sass/scss to library

--- snowpack.config.js
+++ snowpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     public: "/",
     src: "/dist",
   },
-  plugins: ["@snowpack/plugin-react-refresh"],
+  plugins: ["@snowpack/plugin-react-refresh", "@snowpack/plugin-sass"],
   routes: [
     /* Enable an SPA Fallback in development: */
     // {"match": "routes", "src": ".*", "dest": "/index.html"},
