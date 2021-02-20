3da89b679c03e7dcb46d9b08421216382b51b1e4
Civan <civan.erbay@web.de>
2021-02-20 11:47:13 +0100

    Add global breakpoints

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Fabian Lorenz
+        <h2> Fabian Lorenz </h2> 
       </header>
     </div>
   );
