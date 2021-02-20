1c081176d58ace7ddd697e38aa77b1fc129d3995
Civan <civan.erbay@web.de>
2021-02-20 12:44:43 +0100

    Embed grid example

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import React from 'react'
+import Landing from './components/Landing/Landing';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h2> Fabian Lorenz </h2> 
+        <Landing />
       </header>
     </div>
   );
