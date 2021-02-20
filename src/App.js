fc800daaec1a1ee05ab96daeba752a3f15f1f848
Paco Croket <croketproduction@gmail.com>
2021-02-20 11:59:46 +0100

    useScrollTrigger

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import './App.css';
+import "./App.css";
+import { useScrollTrigger } from "./useScrollTrigger";
 
 function App() {
+  const { pageIndex } = useScrollTrigger();
+
+  console.log({ pageIndex });
+
   return (
     <div className="App">
-      <header className="App-header">
-        Fabian Lorenz
-      </header>
+      <div className="stage">
+        <header className="App-header">Fabian Lorenz</header>
+        <div className="mx-auto p-6">
+          {/* Current Scroll: {scrollY} */}
+          Current Page: {pageIndex}
+        </div>
+      </div>
     </div>
   );
 }
