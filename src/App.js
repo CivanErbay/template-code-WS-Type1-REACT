67dd2221c4a31a0cb8832275b7ca6cf7252d78ed
Paco Croket <croketproduction@gmail.com>
2021-02-22 19:14:35 +0100

    pageRenderSwitch

--- src/App.js
+++ src/App.js
@@ -2,18 +2,32 @@ import "./App.css";
 import { useScrollTrigger } from "./useScrollTrigger";
 
 function App() {
-  const { pageIndex } = useScrollTrigger();
+  const { pageIndex, currentPageScrollY } = useScrollTrigger();
 
-  console.log({ pageIndex });
+  console.log({ pageIndex, currentPageScrollY });
+
+  pageRenderSwitch = () => {
+    switch (pageIndex) {
+      case 0:
+        // return top page
+        break;
+
+      default:
+        break;
+    }
+  };
 
   return (
     <div className="App">
+      {/* Add components for header, footer and sides */}
       <div className="stage">
         <header className="App-header">Fabian Lorenz</header>
         <div className="mx-auto p-6">
           {/* Current Scroll: {scrollY} */}
           Current Page: {pageIndex}
         </div>
+        {/* Switch for each page based on scroll */}
+        {pageRenderSwitch()}
       </div>
     </div>
   );
