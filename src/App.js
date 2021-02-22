364bb75e0c23c185b85d17ce6f803b9705fd2100
Paco Croket <croketproduction@gmail.com>
2021-02-22 19:37:02 +0100

    fix return statement

--- src/App.js
+++ src/App.js
@@ -4,32 +4,32 @@ import React from 'react'
 import Landing from './components/Landing/Landing'
 
 function App() {
-    const { pageIndex, currentPageScrollY } = useScrollTrigger()
-    // console.log({ pageIndex, currentPageScrollY });
+  const { pageIndex, currentPageScrollY } = useScrollTrigger()
+  // console.log({ pageIndex, currentPageScrollY });
 
-    const pageRenderSwitch = () => {
-        switch (pageIndex) {
-            case 0:
-                return <Landing />
-            default:
-                ;<Landing />
-        }
+  const pageRenderSwitch = () => {
+    switch (pageIndex) {
+      case 0:
+        return <Landing />
+      default:
+        return <Landing />
     }
+  }
 
-    return (
-        <div className="App">
-            {/* Add components for header, footer and sides */}
-            <div className="stage">
-                <header className="App-header">Fabian Lorenz</header>
-                <div className="mx-auto p-6">
-                    {/* Current Scroll: {scrollY} */}
-                    Current Page: {pageIndex}
-                </div>
-                {/* Switch for each page based on scroll */}
-                {pageRenderSwitch()}
-            </div>
+  return (
+    <div className="App">
+      {/* Add components for header, footer and sides */}
+      <div className="stage">
+        <header className="App-header">Fabian Lorenz</header>
+        <div className="mx-auto p-6">
+          {/* Current Scroll: {scrollY} */}
+          Current Page: {pageIndex}
         </div>
-    )
+        {/* Switch for each page based on scroll */}
+        {pageRenderSwitch()}
+      </div>
+    </div>
+  )
 }
 
 export default App
